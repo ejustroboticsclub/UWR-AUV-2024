5c2f1fdb5dd4fcec0f2ae49579da1fdc328f23ba
amromeshref <amr.khamees@ejust.edu.eg>
2024-09-12 13:13:52 +0300

    update bucket_detection.py

--- auv/sample-insertion/src/detection/bucket_detection.py
+++ auv/sample-insertion/src/detection/bucket_detection.py
@@ -5,7 +5,7 @@ import cv2
 
 MODEL_PATH = "../../models/best-models/last.pt"
 CONFIDENCE_THRESHOLD = 0.1
-RATIO_COLOR_DETECTION_THRESHOLD = 0.8
+RATIO_COLOR_DETECTION_THRESHOLD = 0.3
 
 
 class BucketDetection:
@@ -72,7 +72,130 @@ class BucketDetection:
 
         return image_copy
 
-    def recognize_bucket_color(self, image: np.ndarray, color_correct : bool = False) -> str:
+    def get_bounding_boxes_and_conf_score(self, results) -> tuple[list[list[int]], list[float]]:
+        """
+        Extract bounding boxes and confidence scores from the YOLOv8 results.
+        Args:
+            results: The YOLOv8 results object containing bounding boxes and confidence scores.
+        Returns:
+            list[list[int]]: A list of bounding boxes in the format [x1, y1, x2, y2].
+            list[float]: A list of confidence scores.
+            None, None: If no bucket is detected in the image.
+        """
+        scores = []
+        bboxes = []
+
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                # Filter out non-bucket detections
+                if int(box.cls) != 0:
+                    continue
+
+                # Get the Bounding box coordinates (x1, y1, x2, y2)
+                bounding_boxes = box.xyxy.cpu().numpy()
+
+                # Get the confidence scores
+                score = box.conf.cpu().numpy().tolist()
+                scores.append(score)
+
+                for bounding_box in bounding_boxes:
+                    x1, y1, x2, y2 = map(int, bounding_box)
+                    bboxes.append([x1, y1, x2, y2])
+
+        if len(bboxes) == 0:
+            return None, None
+        else:
+            return bboxes, scores
+
+    def get_direction_color(self, image: np.ndarray, bounding_boxes: list[list[int]], color: str, color_correct: bool = False) -> tuple[int, int]:
+        """
+        Get the direction of the bucket based on the color of the bucket.
+        Args:
+            image (np.ndarray) - BGR format: The original image containing the detected buckets.
+            bounding_boxes (list[list[int]]) - A list of bounding boxes in the format [x1, y1, x2, y2].
+            color (str) - The color of the bucket to be filtered. It can be 'red', 'green', or 'yellow'.
+            color_correct (bool) - Whether to apply color correction to the image before color detection.
+        Returns:
+            tuple[int, int]: The direction of the bucket in the format (x, y).
+            None, None: If no bucket is detected in the image.
+        """
+        x = None
+        y = None
+
+        # Check if there are no bounding boxes
+        if bounding_boxes is None:
+            return x, y
+
+        for bounding_box in bounding_boxes:
+            x1, y1, x2, y2 = bounding_box
+            bucket_image = image[y1:y2, x1:x2]
+            bucket_mask = self.detect_color(bucket_image, color, color_correct)
+            bucket_pixels = cv2.countNonZero(bucket_mask)
+            total_pixels = bucket_image.shape[0] * bucket_image.shape[1]
+            bucket_ratio = bucket_pixels / total_pixels
+            if bucket_ratio >= RATIO_COLOR_DETECTION_THRESHOLD:
+                x = (x1 + x2) // 2
+                y = y1
+                break
+
+        return x, y
+
+    def get_direction_highest_confidence(self, bounding_boxes: list[list[int]], scores: list[float]) -> tuple[int, int]:
+        """
+        Get the direction of the bucket based on the highest confidence score.
+        Args:
+            bounding_boxes (list[list[int]]) - A list of bounding boxes in the format [x1, y1, x2, y2].
+            scores (list[float]) - A list of confidence scores.
+        Returns:
+            tuple[int, int]: The direction of the bucket in the format (x, y).
+            None, None: If no bucket is detected in the image.
+        """
+        x = None
+        y = None
+
+        # Check if there are no bounding boxes
+        if bounding_boxes is None:
+            return x, y
+
+        max_score = max(scores)
+        max_score_index = scores.index(max_score)
+
+        x1, y1, x2, y2 = bounding_boxes[max_score_index]
+        x = (x1 + x2) // 2
+        y = y1
+
+        return x, y
+
+    def get_direction(self, image: np.ndarray, results, filter_criteria: str, color: str = None, color_correct: bool = False) -> tuple[int, int]:
+        """
+        Get the direction of the bucket based on the filter criteria.
+        Args:
+            image (np.ndarray) - BGR format: The original image containing the detected buckets.
+            results: The YOLOv8 results object containing bounding boxes and confidence scores.
+            filter_criteria (str) - The criteria to filter the buckets. It can be 'highest_confidence', or 'color'.
+            color (str) - The color of the bucket to be filtered. It can be 'red', 'green', or 'yellow'.
+            color_correct (bool) - Whether to apply color correction to the image before color detection.
+        Returns:
+            tuple[int, int]: The direction of the bucket in the format (x, y).
+            None, None: If no bucket is detected in the image or no color is detected in the bucket.
+        """
+        bounding_boxes, scores = self.get_bounding_boxes_and_conf_score(
+            results)
+
+        if filter_criteria == "highest_confidence":
+            x, y = self.get_direction_highest_confidence(
+                bounding_boxes, scores)
+            return x, y
+        elif filter_criteria == "color":
+            x, y = self.get_direction_color(
+                image, bounding_boxes, color, color_correct)
+            return x, y
+        else:
+            raise ValueError(
+                "Invalid filter criteria specified. Please specify 'highest_confidence', or 'color'.")
+
+    def recognize_bucket_color(self, image: np.ndarray, color_correct: bool = False) -> str:
         """
         Recognize the color of the bucket in the image by calculating the ratio of non-zero pixels.
         Args:
@@ -92,8 +215,9 @@ class BucketDetection:
         green_pixels = cv2.countNonZero(green_mask)
         yellow_pixels = cv2.countNonZero(yellow_mask)
 
-        print(f"Red pixels: {red_pixels}, Green pixels: {green_pixels}, Yellow pixels: {yellow_pixels}")
-        
+        print(
+            f"Red pixels: {red_pixels}, Green pixels: {green_pixels}, Yellow pixels: {yellow_pixels}")
+
         # Total number of pixels in the image
         total_pixels = image.shape[0] * image.shape[1]
 
@@ -102,10 +226,12 @@ class BucketDetection:
         green_ratio = green_pixels / total_pixels
         yellow_ratio = yellow_pixels / total_pixels
 
-        print(f"Red ratio: {red_ratio:.2f}, Green ratio: {green_ratio:.2f}, Yellow ratio: {yellow_ratio:.2f}")
+        print(
+            f"Red ratio: {red_ratio:.2f}, Green ratio: {green_ratio:.2f}, Yellow ratio: {yellow_ratio:.2f}")
 
         # Store the color ratios in a dictionary
-        color_ratio_dict = {"red": red_ratio, "green": green_ratio, "yellow": yellow_ratio}
+        color_ratio_dict = {"red": red_ratio,
+                            "green": green_ratio, "yellow": yellow_ratio}
 
         # Find the color with the highest ratio
         bucket_color = max(color_ratio_dict, key=color_ratio_dict.get)
@@ -117,7 +243,7 @@ class BucketDetection:
         else:
             return None
 
-    def detect_color(self, image: np.ndarray, color: str, color_correct : bool = False) -> np.ndarray:
+    def detect_color(self, image: np.ndarray, color: str, color_correct: bool = False) -> np.ndarray:
         """
         Detect the specified color in the image.
         Args:
@@ -134,10 +260,10 @@ class BucketDetection:
         elif color == "yellow":
             return self.detect_yellow_color(image, color_correct)
         else:
-            raise ValueError("Invalid color specified. Please specify 'red', 'green', or 'yellow'.")
+            raise ValueError(
+                "Invalid color specified. Please specify 'red', 'green', or 'yellow'.")
 
-
-    def detect_red_color(self, image: np.ndarray, color_correct : bool = False) -> np.ndarray:
+    def detect_red_color(self, image: np.ndarray, color_correct: bool = False) -> np.ndarray:
         """
         Detect red color in the image.
         Args:
@@ -151,7 +277,7 @@ class BucketDetection:
         # Apply color correction to the image if required
         if color_correct:
             image_copy = correct(image_copy)
-        
+
         # Convert the image to the HSV color space
         hsv_image = cv2.cvtColor(image_copy, cv2.COLOR_BGR2HSV)
 
@@ -170,8 +296,8 @@ class BucketDetection:
         combined_mask = lower_mask + upper_mask
 
         return combined_mask
-    
-    def detect_green_color(self, image: np.ndarray, color_correct : bool = False) -> np.ndarray:
+
+    def detect_green_color(self, image: np.ndarray, color_correct: bool = False) -> np.ndarray:
         """
         Detect green color in the image.
         Args:
@@ -185,7 +311,7 @@ class BucketDetection:
         # Apply color correction to the image if required
         if color_correct:
             image_copy = correct(image_copy)
-        
+
         # Convert the image to the HSV color space
         hsv_image = cv2.cvtColor(image_copy, cv2.COLOR_BGR2HSV)
 
@@ -197,8 +323,8 @@ class BucketDetection:
         mask = cv2.inRange(hsv_image, lower_green, upper_green)
 
         return mask
-    
-    def detect_yellow_color(self, image: np.ndarray, color_correct : bool = False) -> np.ndarray:
+
+    def detect_yellow_color(self, image: np.ndarray, color_correct: bool = False) -> np.ndarray:
         """
         Detect yellow color in the image.
         Args:
@@ -255,11 +381,11 @@ class BucketDetection:
             return None
         else:
             return bucket_images
-        
+
 
 if __name__ == "__main__":
     # Load an image
-    image_path = "../../images/image4.jpg"
+    image_path = "../../images/image5.jpg"
     color_correct = True
     image = cv2.imread(image_path)
 
@@ -279,18 +405,19 @@ if __name__ == "__main__":
     # if bucket_images is not None:
     #     for i, bucket_image in enumerate(bucket_images):
     #         cv2.imshow(f"Bucket {i + 1}", bucket_image)
-    
+
     # Detect the red color of the buckets
     if bucket_images is not None:
         for i, bucket_image in enumerate(bucket_images):
-            mask = bucket_detector.detect_color(bucket_image, "red", color_correct)
+            mask = bucket_detector.detect_color(
+                bucket_image, "red", color_correct)
             cv2.imshow("mask"+str(i), mask)
-    
 
     if bucket_images is not None:
         for i, bucket_image in enumerate(bucket_images):
             # Recognize the color of the bucket
-            bucket_color = bucket_detector.recognize_bucket_color(bucket_image, color_correct)
+            bucket_color = bucket_detector.recognize_bucket_color(
+                bucket_image, color_correct)
 
             if bucket_color is not None:
                 print(f"Bucket {i + 1} color: {bucket_color}")
@@ -299,7 +426,7 @@ if __name__ == "__main__":
     else:
         print("No bucket detected in the image.")
 
-    
+
     # Display the image with bounding boxes
     cv2.imshow("Detected Buckets", image_with_boxes)
     cv2.waitKey(0)
